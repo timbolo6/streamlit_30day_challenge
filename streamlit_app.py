a558b6436e87b9852affd79834f50c046511b7c7
Tim Wywiol <tim.leon.wywiol@koerber.com>
2024-09-13 21:33:47 +0200

    Day 29 - Hugging API

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,69 +1,140 @@
 import streamlit as st
-from streamlit_shap import st_shap
-import shap
-from sklearn.model_selection import train_test_split
-import xgboost
-import numpy as np
 import pandas as pd
 
-st.set_page_config(layout="wide")
+# Import for API calls
+import requests
 
-def load_data():
-    return shap.datasets.adult()
+# Import for navbar
+from streamlit_option_menu import option_menu
 
-def load_model(X, y):
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=7)
-    d_train = xgboost.DMatrix(X_train, label=y_train)
-    d_test = xgboost.DMatrix(X_test, label=y_test)
-    params = {
-        "eta": 0.01,
-        "objective": "binary:logistic",
-        "subsample": 0.5,
-        "base_score": np.mean(y_train),
-        "eval_metric": "logloss",
-        "n_jobs": -1,
-    }
-    model = xgboost.train(params, d_train, 10, evals = [(d_test, "test")], verbose_eval=100, early_stopping_rounds=20)
-    return model
+# Import for dynamic tagging
+from streamlit_tags import st_tags, st_tags_sidebar
 
-st.title("`streamlit-shap` for displaying SHAP plots in a Streamlit app")
+# Imports for aggrid
+from st_aggrid import AgGrid
+from st_aggrid.grid_options_builder import GridOptionsBuilder
+from st_aggrid.shared import JsCode
+from st_aggrid import GridUpdateMode, DataReturnMode
 
-with st.expander('About the app'):
-    st.markdown('''[`streamlit-shap`](https://github.com/snehankekre/streamlit-shap) is a Streamlit component that provides a wrapper to display [SHAP](https://github.com/slundberg/shap) plots in [Streamlit](https://streamlit.io/). 
-                    The library is developed by our in-house staff [Snehan Kekre](https://github.com/snehankekre) who also maintains the [Streamlit Documentation](https://docs.streamlit.io/) website.
-                ''')
+# Import for loading interactive keyboard shortcuts into the app
+from gui import keyboard_to_url
+from gui import load_keyboard_class
 
-st.header('Input data')
-X,y = load_data()
-X_display,y_display = shap.datasets.adult(display=True)
 
-with st.expander('About the data'):
-    st.write('Adult census data is used as the example dataset.')
-with st.expander('X'):
-    st.dataframe(X)
-    st.dataframe(X_display)
-with st.expander('y'):
-    st.dataframe(y)
+st.set_page_config(layout='wide', page_title="Zero-Shot Text Classifier", page_icon="🤗")
+st.image("logo.png", width=120)
 
-st.header('SHAP output')
+st.title("Zero-Shot Text Classifier")
 
-# train XGBoost model
-model = load_model(X, y)
+API_KEY = st.secrets["api_token"]
 
-# compute SHAP values
-explainer = shap.Explainer(model, X)
-shap_values = explainer(X)
+API_URL = (
+        "https://api-inference.huggingface.co/models/valhalla/distilbart-mnli-12-3"
+    )
 
-with st.expander('Waterfall plot'):
-    st_shap(shap.plots.waterfall(shap_values[0]), height=300)
-with st.expander('Beeswarm plot'):
-    st_shap(shap.plots.beeswarm(shap_values), height=300)
+headers = {"Authorization": f"Bearer {API_KEY}"}
 
-explainer = shap.TreeExplainer(model)
-shap_values = explainer.shap_values(X)
+label_widget = st_tags(
+        label="",
+        text="Add labels - 3 max",
+        value=["Positive", "Neutral", "Negative"],
+        suggestions=[
+            "Navigational",
+            "Transactional",
+            "Informational",
+            "Positive",
+            "Negative",
+            "Neutral",
+        ],
+        maxtags=3,
+    )
 
-with st.expander('Force plot'):
-    st.subheader('First data instance')
-    st_shap(shap.force_plot(explainer.expected_value, shap_values[0,:], X_display.iloc[0,:]), height=200, width=1000)
-    st.subheader('First thousand data instance')
-    st_shap(shap.force_plot(explainer.expected_value, shap_values[:1000,:], X_display.iloc[:1000,:]), height=400, width=1000)
+new_line = "\n"
+nums = [
+    "Bruv, this new trainers got me walking like I'm headlining London Fashion Week - feeling like a snack and a half",
+    "Fam, that sandwich was so dry, even the desert would be like 'nah, you're good, mate'",
+    "The Party was alright, the DJ need to be called lowrider"
+]
+
+sample = f"{new_line.join(map(str, nums))}"
+MAX_LINES = 5
+text = st.text_area(
+            "Enter keyphrase to classify",
+            sample,
+            height=200,
+            key="2",
+            help="At least two keyphrases for the classifier to work, one per line, "
+            + str(MAX_LINES)
+            + " keyphrases max as part of the demo",
+        )
+lines = text.split("\n")  # A list of lines
+linesList = []
+for x in lines:
+    linesList.append(x)
+linesList = list(dict.fromkeys(linesList))  # Remove dupes
+linesList = list(filter(None, linesList))  # Remove empty
+
+
+if len(linesList) > MAX_LINES:
+
+
+    st.info(
+        f"🚨 Only the first "
+        + str(MAX_LINES)
+        + " keyprases will be reviewed. Unlock that limit by switching to 'Unlocked Mode'"
+    )
+
+def query(payload):
+    response = requests.post(API_URL, headers=headers, json=payload)
+    # Unhash to check status codes from the API response
+    # st.write(response.status_code)
+    return response.json()
+listToAppend = []
+for row in linesList:
+    api_answer = query(
+                {
+                    "inputs": row,
+                    "parameters": {"candidate_labels": label_widget},
+                    "options": {"wait_for_model": True},
+                }
+            )
+    listToAppend.append(api_answer)
+
+df = pd.DataFrame.from_dict(listToAppend)
+
+gb = GridOptionsBuilder.from_dataframe(df)
+# enables pivoting on all columns, however i'd need to change ag grid to allow export of pivoted/grouped data, however it select/filters groups
+gb.configure_default_column(
+    enablePivot=True, enableValue=True, enableRowGroup=True
+)
+gb.configure_selection(selection_mode="multiple", use_checkbox=True)
+gb.configure_side_bar()  # side_bar is clearly a typo :) should by sidebar
+gridOptions = gb.build()
+
+response = AgGrid(
+    df,
+    gridOptions=gridOptions,
+    enable_enterprise_modules=True,
+    update_mode=GridUpdateMode.MODEL_CHANGED,
+    data_return_mode=DataReturnMode.FILTERED_AND_SORTED,
+    height=400,
+    fit_columns_on_grid_load=False,
+    configure_side_bar=True,
+)
+    
+
+@st.cache_data
+def convert_df(df):
+    # IMPORTANT: Cache the conversion to prevent computation on every rerun
+    return df.to_csv().encode("utf-8")
+
+csv = convert_df(df)
+
+st.caption("")
+
+st.download_button(
+    label="Download results as CSV",
+    data=csv,
+    file_name="results.csv",
+    mime="text/csv",
+)
